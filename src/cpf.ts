31c4503e530bc86851ed781ab3928ec617288c4e
ericdaher <ericdaher2@gmail.com>
2023-02-01 09:07:49 -0300

    fix: correction in digits comparison

--- src/cpf.ts
+++ src/cpf.ts
@@ -1,29 +1,16 @@
 // @ts-nocheck
 export function validate (cpf) {
-    if (!contentAndLengthValid(cpf)) return false;
     cpf = removeNonNumbers(cpf);
-    if (cpf.length == "") return false;
-    try {
-        const { cpfDigit1, cpfDigit2 } = sumCpfDigits(cpf);
-        const verificationDigits = cpf.substring(cpf.length - 2, cpf.length);
-        return verificationDigits == getVerificationDigits(cpfDigit1, cpfDigit2);
-    } catch (e) {
-        return false;
-    }
-}
-
-function getVerificationDigits(cpfDigit1, cpfDigit2) {
-    const verificationDigit1 = calculateVerificationDigit(cpfDigit1);
-    cpfDigit2 += 2 * verificationDigit1;
-    const verificationDigit2 = calculateVerificationDigit(cpfDigit2);
-    return `${verificationDigit1}${verificationDigit2}`;
+    if (cpf.length !== 11) return false;
+    const { cpfDigit1, cpfDigit2 } = sumCpfDigits(cpf);
+    const verificationDigits = cpf.substring(cpf.length - 2, cpf.length);
+    return verificationDigits == getVerificationDigits(cpfDigit1, cpfDigit2);
 }
 
 function sumCpfDigits(cpf) {
     let cpfDigit1 = 0, cpfDigit2 = 0;
     for (let digitPosition = 1; digitPosition < cpf.length - 1; digitPosition++) {
         const digit = parseInt(cpf.substring(digitPosition - 1, digitPosition));
-        if (isNaN(digit)) return false;
         ({ cpfDigit1, cpfDigit2 } = accumulateDigits(digit, cpfDigit1, cpfDigit2, digitPosition));
     }
     return { cpfDigit1, cpfDigit2 };
@@ -35,20 +22,24 @@ function accumulateDigits(digit, cpfDigit1, cpfDigit2, digitPosition) {
     return { cpfDigit1, cpfDigit2 };
 }
 
+function getVerificationDigits(cpfDigit1, cpfDigit2) {
+    const verificationDigit1 = calculateVerificationDigit(cpfDigit1);
+    cpfDigit2 += 2 * verificationDigit1;
+    const verificationDigit2 = calculateVerificationDigit(cpfDigit2);
+    return `${verificationDigit1}${verificationDigit2}`;
+}
+
 function calculateVerificationDigit(d) {
     const remainder = d % 11;
     return (remainder < 2) ? 0 : 11 - remainder;
 }
 
 function removeNonNumbers(cpf) {
+    if (cpf == null || cpf == undefined) return '';
     const regExp = /(([0-9]{3})[\.]?([0-9]{3})[\.]?([0-9]{3})[-]?([0-9]{2}))/;
     try {
         return regExp.exec(cpf).slice(2, 6).join('');
     } catch(e) {
         return "";
     }
-}
-
-function contentAndLengthValid(cpf) {
-    return cpf !== null && cpf !== undefined && (cpf.length >= 11 && cpf.length <= 14);
 }
--- test/cpf.test.ts
+++ test/cpf.test.ts
@@ -1,15 +1,15 @@
 const cpf = require("../src/cpf");
 
 test("it returns true when cpf is valid", () => {
-  expect(cpf.validate("66063316507")).toStrictEqual(true);
-  expect(cpf.validate("660.633.165-07")).toStrictEqual(true);
+  expect(cpf.validate("66063316507")).toBe(true);
+  expect(cpf.validate("660.633.165-07")).toBe(true);
 });
 
 test("it returns false when cpf is invalid", () => {
-  expect(cpf.validate("dsadsada")).toStrictEqual(false);
-  expect(cpf.validate("66063316531")).toStrictEqual(false);
-  expect(cpf.validate("66063316531ssss")).toStrictEqual(false);
-  expect(cpf.validate("660-16507s")).toStrictEqual(false);
-  expect(cpf.validate("66.063..316..50.7")).toStrictEqual(false);
-  expect(cpf.validate()).toStrictEqual(false);
+  expect(cpf.validate("dsadsada")).toBe(false);
+  expect(cpf.validate("66063316531")).toBe(false);
+  expect(cpf.validate("66063316531ssss")).toBe(false);
+  expect(cpf.validate("660-16507s")).toBe(false);
+  expect(cpf.validate("66.063..316..50.7")).toBe(false);
+  expect(cpf.validate()).toBe(false);
 });
